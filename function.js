6b3a3bee0172bc8a245bb62e1b24c8777345ee78
Sarthak Dawane <96832694+SarthakDawane@users.noreply.github.com>
2023-11-01 10:04:49 +0530

    functionality added

---
+++ function.js
@@ -0,0 +1,19 @@
+var sidemenu = document.getElementById("sidemenu")
+function openmenu(){
+    sidemenu.style.right = "0" 
+}
+function closemenu(){
+    sidemenu.style.right = "-400px" 
+}
+
+var icon = document.getElementById("icon")
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "./IMAGES/moon.png";
+    }
+    else{
+        icon.src = "./IMAGES/sun.png"
+    }
+}
