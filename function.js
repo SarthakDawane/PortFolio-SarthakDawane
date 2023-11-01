d73eef587e24f145d04a50f2cec5a35c21b91d6c
Sarthak Dawane <96832694+SarthakDawane@users.noreply.github.com>
2023-10-31 20:37:23 -0800

    Update function.js

--- function.js
+++ function.js
@@ -11,9 +11,9 @@ var icon = document.getElementById("icon")
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        icon.src = "./IMAGES/moon.png";
+        icon.src = "IMAGES/moon.png";
     }
     else{
-        icon.src = "./IMAGES/sun.png"
+        icon.src = "IMAGES/sun.png"
     }
 }
